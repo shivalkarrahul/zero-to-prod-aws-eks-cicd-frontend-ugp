ad397abf992057446b47d36827113564e730d4c1
Rahul Shivalkar <rahulshivalkar@rediffmail.com>
2025-08-07 16:48:06 +0530

    Enhance UI for funny quote generator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// You were importing './index.css', which contained uncompiled Tailwind directives.
+// This now imports the newly generated file with the compiled CSS.
+import './tailwind.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
